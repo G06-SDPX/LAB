8e5ea147932ea27d2aed22019ef38c72cc8c6feb
Chidsanu_ <Chidsanupongboonma@gmail.com>
2023-10-11 22:24:45 +0700

    6

--- app/app.py
+++ app/app.py
@@ -13,11 +13,11 @@ def hello():
 @app.route('/is_prime/<int:x>')
 def is_prime(x):
     if x <= 1:
-        return False
+        return 'False'
     for i in range(2, int(x**(1/2)) + 1):
         if (x % i) == 0:
-            return False
-    return True
+            return 'False'
+    return 'True'
 
 
 if __name__ == '__main__':
